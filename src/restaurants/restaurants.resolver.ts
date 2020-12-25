1361b073fed36fdfb7feb99d6ec22fb90513a9b9
RyuJeongSang <42318201+RyuJeongSang@users.noreply.github.com>
2020-12-25 23:24:19 +0900

    user done

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,4 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { number } from 'joi';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserResolver } from './users.resolver';
+import { UserSerVice } from './users.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserResolver, UserSerVice],
+  exports: [UserSerVice],
+})
 export class UsersModule {}
